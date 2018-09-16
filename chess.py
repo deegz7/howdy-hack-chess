209aeed22ccd874bbe2a45df85536a1f0afbabb0
Jack Smith <jsmithtech2019@gmail.com>
2018-09-15 19:06:54 -0500

    Update chess.py

--- chess.py
+++ chess.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python
+import pygame
 
+pygame.init()
+'''
 class ChessBoard:
     """docstring for ChessBoard"""
     def __init__(self):
@@ -29,6 +31,11 @@ class ChessBoard:
         game_board[7][6] = Knight("b")
         game_board[7][7] = Rook("b")
 
+        # Initialize pawns
+        for i in range(8):
+            game[1][i] = Pawn("w")
+            game[6][i] = Pawn("b")
+
 
 class Pawn():
     def __init__(self, arg):
@@ -36,6 +43,15 @@ class Pawn():
         self.piece = self.create_pawn()
 
     def create_pawn():
+
+    def move():
+        # First move can be 2 forward
+
+        # Moves 1 step forward
+
+        # Attack left
+
+        # Attack right
 
 class Knight():
     def __init__(self):
@@ -77,3 +93,58 @@ class Game(object):
         Rook.__init__(self)
         Knight.__init__(self)
         Pawn.__init__(self)
+'''
+
+# Images
+class Board(pygame.sprite.Sprite):
+    def __init__(self, image_file, location):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(image_file)
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
+#Img = pygame.image.load('.png')
+#Img = pygame.image.load('.png')
+#Img = pygame.image.load('.png')
+#Img = pygame.image.load('.png')
+#Img = pygame.image.load('.png')
+
+# value defs
+
+
+
+# *********************************************************************
+# Start the code
+
+# Create display board
+board = Board('chess_board.png', [0,0])
+display_width = 1000
+display_height = 1000
+chessDisplay = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('Get It Off Your Chess')
+chessDisplay.fill([255,255,255])
+chessDisplay.blit(board.image, board.rect)
+
+# Clock
+clock = pygame.time.Clock()
+
+# Define color schemes
+black = (0,0,0)
+white = (255,255,255)
+maroon = (80,0,0)
+
+
+
+checkMate = False
+while not checkMate:
+    for event in pygame.event.get():
+        #if event.type == pygame.QUIT:
+            # They closed it
+        print(event)
+
+    pygame.display.update()
+
+    clock.tick(30)
+
+pygame.quit()
+
+quit()
